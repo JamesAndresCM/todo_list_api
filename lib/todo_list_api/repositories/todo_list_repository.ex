a5a7f6348baefb54f6df22d2c4747a145562a0c3
jcanales <jcanales@webdoxclm.com>
2024-08-08 23:34:35 -0400

    feat: create methods in repository pattern

--- lib/todo_list_api/repositories/todo_list_repository.ex
+++ lib/todo_list_api/repositories/todo_list_repository.ex
@@ -3,4 +3,13 @@ defmodule Repositories.TodoListRepository do
   def all() do
     Repo.all(TodoList)
   end
+
+  def find_by_id!(id) when is_binary(id) do
+    Repo.get!(TodoList, id)
+  end
+  def find_by_id!(_), do: nil
+
+  def create(params) do
+    %TodoList{} |> TodoList.changeset(params) |> Repo.insert
+  end
 end
