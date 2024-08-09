5b853c369662a100bcf2b07b24a41139096a140d
jcanales <jcanales@webdoxclm.com>
2024-08-08 23:34:48 -0400

    feat: create serializers

---
+++ lib/todo_list_api/serializers/todo_list_json.ex
@@ -0,0 +1,18 @@
+defmodule Serializers.TodoListJSON do
+  def index(todo_lists) do
+    %{data: for(el <- todo_lists, do: data(el))}
+  end
+
+  def show(todo_list) do
+    %{data: data(todo_list)}
+  end
+
+  defp data(todo_list) do
+    %{
+      id: todo_list.id,
+      name: todo_list.name,
+      description: todo_list.description,
+      status: todo_list.status
+    }
+  end
+end
