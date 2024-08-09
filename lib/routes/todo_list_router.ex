c6de31b02067c53b1f7902b82b1141581cf71d56
jcanales <jcanales@webdoxclm.com>
2024-08-08 23:33:46 -0400

    feat: added method create and show

--- lib/routes/todo_list_router.ex
+++ lib/routes/todo_list_router.ex
@@ -1,6 +1,7 @@
 defmodule Routes.TodoListRouter do
   use Plug.Router
   alias Repositories.TodoListRepository
+  alias Serializers.{TodoListJSON, ChangesetJSON}
   plug :match
 
   # We want the response to be sent in json format.
@@ -8,14 +9,32 @@ defmodule Routes.TodoListRouter do
 
   plug :dispatch
 
-  @users [%{id: 1, name: "jaime"},  %{id: 2, name: "andres"}]
-
   get "/todo_lists" do
-    send_resp(conn, 200, Jason.encode!(%{data: render_all()}))
+    todo_lists = TodoListRepository.all() |> TodoListJSON.index
+    send_resp(conn, 200, Jason.encode!(todo_lists))
   end
 
+  get "/todo_lists/:id" do
+    try do
+      todo_list = TodoListRepository.find_by_id!(id)
+      send_resp(conn, 200, Jason.encode!(todo_list |> TodoListJSON.show))
+    rescue _ ->
+      send_resp(conn, 404, Jason.encode!(%{error: "record not found"}))
+    end
+  end
 
-  defp render_all do
-    for n <- TodoListRepository.all(), do: %{id: n.id, name: n.name, description: n.description, status: n.status}
+  post "/todo_lists" do
+    params = Map.get(conn.body_params, "todo_list", %{})
+    case TodoListRepository.create(params) do
+      {:ok, todo_list} ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, Jason.encode!(todo_list |> TodoListJSON.show))
+      {:error, changeset} ->
+        errors = ChangesetJSON.error(%{changeset: changeset})
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(422, Jason.encode!(errors))
+    end
   end
 end
