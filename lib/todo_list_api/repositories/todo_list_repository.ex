7b8164e2c59cf7afa38262b3a6b68c8602dec66a
jcanales <jcanales@webdoxclm.com>
2024-08-13 23:25:09 -0400

    feat: create update and destroy methods

--- lib/todo_list_api/repositories/todo_list_repository.ex
+++ lib/todo_list_api/repositories/todo_list_repository.ex
@@ -4,12 +4,24 @@ defmodule Repositories.TodoListRepository do
     Repo.all(TodoList)
   end
 
-  def find_by_id!(id) when is_binary(id) do
+  def find_by_id!(id) do
     Repo.get!(TodoList, id)
   end
-  def find_by_id!(_), do: nil
 
   def create(params) do
     %TodoList{} |> TodoList.changeset(params) |> Repo.insert
+  end
+
+  def update(todo_list = %TodoList{}, params) do
+    todo_list |> TodoList.changeset(params) |> Repo.update()
+  end
+
+  def destroy(id) do
+    try do
+     todo_list = find_by_id!(id)
+     Repo.delete(todo_list)
+    rescue _ ->
+      {:error, "record not found"}
+    end
   end
 end
