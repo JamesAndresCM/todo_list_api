225642049250ebf7d6aed9c188a9c3cc91808141
jcanales <jcanales@webdoxclm.com>
2024-08-21 23:35:40 -0400

    feat: added user & pass

--- config/test.exs
+++ config/test.exs
@@ -3,6 +3,8 @@ import Config
 config :todo_list_api, TodoListDB.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "pass",
   database: "todo_list_test",
   hostname: "localhost"
 
