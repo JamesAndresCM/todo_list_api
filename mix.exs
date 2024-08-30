cba62c696039375b76e19bcc14f2120f7ba876f9
JamesACM <jaimecanalesmarin@gmail.com>
2024-08-29 22:41:16 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule TodoListApi.MixProject do
 
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"]
     ]
   end
